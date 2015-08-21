cbc4ab26f070f7ecbcc48917828a80d3dc625ae3
Joao Duarte <jsvduarte@gmail.com>
2015-08-21 15:43:00 +0100

    add specs

--- logstash-output-http.gemspec
+++ logstash-output-http.gemspec
@@ -25,5 +25,6 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'ftw', ['~> 0.0.40']
 
   s.add_development_dependency 'logstash-devutils'
+  s.add_development_dependency 'logstash-codec-plain'
 end
 
--- spec/outputs/http_spec.rb
+++ spec/outputs/http_spec.rb
@@ -1,1 +1,41 @@
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/outputs/http"
+require "ftw"
+
+
+class LogStash::Outputs::Http
+  attr_writer :agent
+end
+
+describe LogStash::Outputs::Http do
+  let(:agent) { FTW::Agent.new }
+  let(:event) { LogStash::Event.new("message" => "hi") }
+  let(:url) { "http://localhost:3131" }
+  let(:method) { "post" }
+  subject { LogStash::Outputs::Http.new("url" => url, "http_method" => method) }
+
+  before :each do
+    subject.register
+    subject.agent = agent
+  end
+
+  it "should execute a request" do
+    expect(agent).to receive(:execute).with(FTW::Request)
+    subject.receive(event)
+  end
+
+  context "http_method = post" do
+    it "should execute a POST to a url" do
+      expect(agent).to receive(:post).with(url).and_call_original
+      subject.receive(event)
+    end
+  end
+
+  context "http_method = put" do
+    let(:method) { "put" }
+    it "should execute a PUT to a url" do
+      expect(agent).to receive(:put).with(url).and_call_original
+      subject.receive(event)
+    end
+  end
+end
