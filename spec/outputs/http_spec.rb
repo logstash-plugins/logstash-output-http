131981f9acafe033aeeea1ff6b66a91a7649a9c4
Andrew Cholakian <andrew@andrewvc.com>
2015-08-25 11:20:12 -0500

    Add integration specs, fix issues with form encoding

--- spec/outputs/http_spec.rb
+++ spec/outputs/http_spec.rb
@@ -10,26 +10,34 @@ class LogStash::Outputs::Http
   attr_reader :request_tokens
 end
 
-RSpec.configure do |config|
-  class TestApp < Sinatra::Base
-    def self.multiroute(methods, path, &block)
-      methods.each do |method|
-        method.to_sym
-        self.send method, path, &block
-      end
-    end
-
-
-
-    multiroute(%w(get post put patch delete), "/good") do
-      [200, "YUP"]
-    end
-
-    multiroute(%w(get post put patch delete), "/bad") do
-      [500, "YUP"]
+class TestApp < Sinatra::Base
+  def self.multiroute(methods, path, &block)
+    methods.each do |method|
+      method.to_sym
+      self.send method, path, &block
     end
   end
 
+  def self.last_request=(request)
+    @last_request = request
+  end
+
+  def self.last_request
+    @last_request
+  end
+
+  multiroute(%w(get post put patch delete), "/good") do
+    self.class.last_request = request
+    [200, "YUP"]
+  end
+
+  multiroute(%w(get post put patch delete), "/bad") do
+    self.class.last_request = request
+    [500, "YUP"]
+  end
+end
+
+RSpec.configure do |config|
   #http://stackoverflow.com/questions/6557079/start-and-call-ruby-http-server-in-the-same-script
   def sinatra_run_wait(app, opts)
     queue = Queue.new
@@ -49,6 +57,15 @@ RSpec.configure do |config|
 end
 
 describe LogStash::Outputs::Http do
+  # Wait for the async request to finish in this spinlock
+  # Requires pool_max to be 1
+  def wait_for_request
+
+    loop do
+      break if subject.request_tokens.size > 0
+    end
+  end
+
   let(:port) { PORT }
   let(:event) { LogStash::Event.new("message" => "hi") }
   let(:url) { "http://localhost:#{port}/good" }
@@ -119,9 +136,7 @@ describe LogStash::Outputs::Http do
 
         before do
           subject.receive(event)
-          loop do
-            break if subject.request_tokens.size > 0
-          end
+          wait_for_request
         end
 
         it "should log a failure" do
@@ -134,6 +149,84 @@ describe LogStash::Outputs::Http do
   LogStash::Outputs::Http::VALID_METHODS.each do |method|
     context "when using '#{method}'" do
       include_examples("verb behavior", method)
+    end
+  end
+
+  shared_examples("a received event") do
+    before do
+      TestApp.last_request = nil
+    end
+
+    before do
+      subject.receive(event)
+      wait_for_request
+    end
+
+    let(:last_request) { TestApp.last_request }
+    let(:body) { last_request.body.read }
+    let(:content_type) { last_request.env["CONTENT_TYPE"] }
+
+    it "should receive the request" do
+      expect(last_request).to be_truthy
+    end
+
+    it "should receive the event as a hash" do
+      expect(body).to eql(expected_body)
+    end
+
+    it "should have the correct content type" do
+      expect(content_type).to eql(expected_content_type)
+    end
+  end
+
+  describe "integration tests" do
+    let(:url) { "http://localhost:#{port}/good" }
+    let(:event) { LogStash::Event.new("foo" => "bar", "baz" => "bot")}
+
+    subject { LogStash::Outputs::Http.new(config) }
+
+    before do
+      subject.register
+    end
+
+    describe "sending with the default (JSON) config" do
+      let(:config) {
+        {"url" => url, "http_method" => "post", "pool_max" => 1}
+      }
+      let(:expected_body) { LogStash::Json.dump(event) }
+      let(:expected_content_type) { "application/json" }
+
+      include_examples("a received event")
+    end
+
+    describe "sending the event as a form" do
+      let(:config) {
+        {"url" => url, "http_method" => "post", "pool_max" => 1, "format" => "form"}
+      }
+      let(:expected_body) { subject.send(:encode, event.to_hash) }
+      let(:expected_content_type) { "application/x-www-form-urlencoded" }
+
+      include_examples("a received event")
+    end
+
+    describe "sending the event as a message" do
+      let(:config) {
+        {"url" => url, "http_method" => "post", "pool_max" => 1, "format" => "message", "message" => "%{foo} AND %{baz}"}
+      }
+      let(:expected_body) { "#{event["foo"]} AND #{event["baz"]}" }
+      let(:expected_content_type) { "text/plain" }
+
+      include_examples("a received event")
+    end
+
+    describe "sending a mapped event" do
+      let(:config) {
+        {"url" => url, "http_method" => "post", "pool_max" => 1, "mapping" => {"blah" => "X %{foo}"}}
+      }
+      let(:expected_body) { LogStash::Json.dump("blah" => "X #{event["foo"]}") }
+      let(:expected_content_type) { "application/json" }
+
+      include_examples("a received event")
     end
   end
 end
