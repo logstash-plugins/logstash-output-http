eb5574c9a1e3230b44a64e18cc84b909fd6a0c93
Colin Surprenant <colin.surprenant@gmail.com>
2015-10-13 15:47:15 -0400

    silence request logs, add robustness to thread, add sleep in spin loop

--- spec/outputs/http_spec.rb
+++ spec/outputs/http_spec.rb
@@ -10,7 +10,19 @@ class LogStash::Outputs::Http
   attr_reader :request_tokens
 end
 
+# note that Sinatra startup and shutdown messages are directly logged to stderr so
+# it is not really possible to disable them without reopening stderr which is not advisable.
+#
+# == Sinatra (v1.4.6) has taken the stage on 51572 for development with backup from WEBrick
+# == Sinatra has ended his set (crowd applauds)
+
 class TestApp < Sinatra::Base
+
+  # disable WEBrick logging
+  def self.server_settings
+    { :AccessLog => [], :Logger => WEBrick::BasicLog::new(nil, WEBrick::BasicLog::FATAL) }
+  end
+
   def self.multiroute(methods, path, &block)
     methods.each do |method|
       method.to_sym
@@ -41,15 +53,19 @@ RSpec.configure do |config|
   #http://stackoverflow.com/questions/6557079/start-and-call-ruby-http-server-in-the-same-script
   def sinatra_run_wait(app, opts)
     queue = Queue.new
-    thread = Thread.new do
-      Thread.abort_on_exception = true
-      app.run!(opts) do |server|
-        queue.push("started")
+
+    Thread.new(queue) do |queue|
+      begin
+        app.run!(opts) do |server|
+          queue.push("started")
+        end
+      rescue
+        # ignore
       end
     end
+
     queue.pop # blocks until the run! callback runs
   end
-
 
   config.before(:suite) do
     sinatra_run_wait(TestApp, :port => PORT, :server => 'webrick')
@@ -62,6 +78,7 @@ describe LogStash::Outputs::Http do
   def wait_for_request
 
     loop do
+      sleep(0.1)
       break if subject.request_tokens.size > 0
     end
   end
